b2b0e97c0ca6781e04eead48a3c530851823b20a
Mathieu Marquis <mathieu.marquis.2@ulaval.ca>
2023-05-29 09:50:29 -0400

    get_center_point done

--- gaussian_fitting/cube_analyzer.py
+++ gaussian_fitting/cube_analyzer.py
@@ -9,8 +9,6 @@ from cube_spectrum import Spectrum
 # data_cube = np.flip(fits.open("calibration.fits")[0].data, axis=1)
 data_cube = fits.open("calibration.fits")[0].data
 
-test = data_cube[30,:,:]
-
 # MIDDLE PIXEL (522,489)
 
 # plt.imshow(data_cube[0,:,:])
@@ -20,26 +18,43 @@ class Calibration_cube_analyzer():
     def __init__(self, data_cube_file_name=str):
         self.data_cube = (fits.open(data_cube_file_name)[0]).data
 
-    def find_center_point(self):
-        center_guess = 511, 511
-        distances = {"x": {}, "y": {}}
-        for channel in range(48):
+    def get_center_point(self):
+        center_guess = 527, 484
+        distances = {"x": [], "y": []}
+        success = 0
+        for channel in range(1,49):
             print("channel:", channel)
-            channel_pos = {}
-            intensity_x_list = self.data_cube[channel, center_guess[1], :]
-            intensity_y_list = self.data_cube[channel, :, center_guess[0]]
-            for axes in range(2):
-                last_intensity = 0
+            channel_dist = {}
+            intensity_max = {
+                "intensity_max_x": self.data_cube[channel-1, center_guess[1], :],
+                "intensity_max_y": self.data_cube[channel-1, :, center_guess[0]]
+            }
+            for name, axe_list in intensity_max.items():
                 axes_pos = []
-                for coord in range(1,1024):
-                    if intensity_x_list[coord-1] < intensity_x_list[coord] > intensity_x_list[coord+1] and intensity_x_list[coord] > 500:
-                        axes_pos.append(coord - 1)
-                    # new_intensity = intensity_x_list[coord]
-                    # if new_intensity > 500 and new_intensity < last_intensity:
-                    #     axes_pos.append(coord - 1)
-                    # last_intensity = new_intensity
-                print(axes_pos)
-                raise ArithmeticError
+                for coord in range(1, 1023):
+                    if (axe_list[coord-1] < axe_list[coord] > axe_list[coord+1]
+                         and axe_list[coord] > 500):
+                        axes_pos.append(coord)
+                # Verification of single maxes
+                for i in range(len(axes_pos)-1):
+                    if axes_pos[i+1] - axes_pos[i] < 50:
+                        if axe_list[axes_pos[i]] > axe_list[axes_pos[i+1]]:
+                            axes_pos[i+1] = 0
+                        else:
+                            axes_pos[i] = 0
+                axes_pos = np.setdiff1d(axes_pos, 0)
+                
+                if len(axes_pos) == 4:
+                    channel_dist[name] = [(axes_pos[3] - axes_pos[0])/2 + axes_pos[0], (axes_pos[2] - axes_pos[1])/2 + axes_pos[1]]
+                    distances[name[-1]].append(channel_dist[name])
+                    success += 1
+                else:
+                    print(f"channel {channel} not len == 4, len == {len(axes_pos)}, {axes_pos}")
+            # print(distances)
+        x_mean, y_mean = np.mean(distances["x"], axis=(0,1)), np.mean(distances["y"], axis=(0,1))
+        print(x_mean, y_mean)
+        print("success=", success)
+        return x_mean, y_mean
 
 
 
@@ -63,7 +78,7 @@ class Calibration_cube_analyzer():
 
     def get_FWHM_mean(self, px_radius):
         # Storage of the Fabry-Perot's center point on the image
-        image_center_x, image_center_y = 522, 489
+        image_center_x, image_center_y = 527, 483
         # Storage of the center point in the data array: the x and y coordinates are swapped
         center_x, center_y = image_center_y, image_center_x
 
@@ -91,16 +106,17 @@ class Calibration_cube_analyzer():
 
 
 analyzer = Calibration_cube_analyzer("calibration.fits")
-analyzer.find_center_point()
+analyzer.get_center_point()
+# analyzer.get_FWHM_mean(5)
 
 
 
-
-# masked = np.ma.masked_less(test, 610)
-# print(masked[489,522])
-# plt.imshow(masked, origin="lower")
-# plt.plot()
-# plt.show()
+test = data_cube[35,:,:]
+masked = np.ma.masked_less(test, 610)
+print(masked[489,522])
+plt.imshow(test, origin="lower")
+plt.plot(527,484,marker="v",color="white")
+plt.show()
 
 x, y = 500, 500
 
