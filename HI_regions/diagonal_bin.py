f30d8bb6d40fffa14071b239aa7a773979c2b7b3
Mathieu Marquis <mathieu.marquis.2@ulaval.ca>
2023-07-25 11:24:14 -0400

    switch_axes, get_switched_header

--- HI_regions/diagonal_bin.py
+++ HI_regions/diagonal_bin.py
@@ -10,6 +10,7 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astropy import units as u
 from PIL import Image
+from copy import deepcopy
 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
@@ -84,18 +85,40 @@ class Data_cube(Fits_file):
     Encapsulate all the useful methods for the analysis of a data cube.
     """
 
-    def __init__(self, fits_object: fits.PrimaryHDU):
+    def __init__(self, fits_object: fits.PrimaryHDU, axes_info: dict={"x": "l", "y": "b", "z": "v"}):
         """
         Initialize an analyzer object. The datacube's file name must be given.
 
         Arguments
         ---------
         fits_object: astropy.io.fits.hdu.image.PrimaryHDU. Contains the data values and header of the data cube.
+        axes_info: tuple of str, default=("longitude l", "latitude b", "spectral axis"). If present, specifies what is
+        represented by which axis and the three str then represent the x, y and z axes respectively. This argument is stored
+        in the info attribute.
         """
         self.object = fits_object
         self.data = fits_object.data
         self.header = fits_object.header
+        self.info = axes_info
 
+    def __str__(self):
+        return f"Data_cube shape: {self.data.shape}\nData_cube axes: {list(reversed(self.info.items()))}"
+
+    def get_header_without_third_dimension(self) -> fits.header:
+        """
+        Get the adaptation of a Data_cube object's header for a Map object by removing the spectral axis.
+
+        Returns
+        -------
+        astropy.io.fits.header.Header: header with the same but with the third axis removed.
+        """
+        header = self.header.copy()
+        wcs = WCS(header)
+        wcs.sip = None
+        wcs = wcs.dropaxis(2)
+        header = wcs.to_header(relax=True)
+        return header
+    
     def bin_cube(self, nb_pix_bin: int=2) -> Data_cube:
         """
         Bin a specific cube by the amount of pixels given for every channel.
@@ -137,6 +160,10 @@ class Data_cube(Fits_file):
         # The mean value of every pixel group at every channel is calculated and the array returns to a three dimensional state
         return Data_cube(fits.PrimaryHDU(np.nanmean(bin_array, axis=(2,4)), self.bin_header(nb_pix_bin)))
 
+    def rotate(self, angle) -> Data_cube:
+        rotated_data = scipy.ndimage.rotate(self.data.swapaxes(0,2).swapaxes(0,1), angle=angle)
+        return Data_cube(fits.PrimaryHDU(rotated_data.swapaxes(0,1).swapaxes(0,2), self.header))
+
     def bin_cube_diagonally(self, nb_pix_bin: int, angle: float) -> Data_cube:
         """
         Bin a Data_cube diagonally. This takes the values of the cube in a diagonal manner.
@@ -149,32 +176,63 @@ class Data_cube(Fits_file):
         crop_pix = np.floor((new_shape[1:] - old_shape[1:]) / 2)
         slices = np.array((crop_pix[0]+1, new_shape[1]-crop_pix[0]-1,crop_pix[1]+1,new_shape[2]-crop_pix[1]-1)).astype(int)
         return Data_cube(fits.PrimaryHDU(rotated_data[:,slices[0]:slices[1],slices[2]:slices[3]], self.bin_header(nb_pix_bin)))
-
-    def rotate(self, angle) -> Data_cube:
-        rotated_data = scipy.ndimage.rotate(self.data.swapaxes(0,2).swapaxes(0,1), angle=angle)
-        return Data_cube(fits.PrimaryHDU(rotated_data.swapaxes(0,1).swapaxes(0,2), self.header))
     
     def plot_cube(self):
         plt.imshow(self.data[13,:,:], origin="lower")
         plt.show()
 
-    def get_header_without_third_dimension(self) -> fits.header:
-        """
-        Get the adaptation of a Data_cube object's header for a Map object by removing the spectral axis.
+    def switch_axes(self, new_axes: dict) -> Data_cube:
+        new_data = np.copy(self.data)
+        new_header = self.header.copy()
+        old_axes_pos = dict(zip((self.info.values()), (0,1,2)))
+        new_axes_pos = dict(zip((new_axes.values()), (0,1,2)))
+        dict_keys = list(new_axes_pos.keys())
 
-        Returns
-        -------
-        astropy.io.fits.header.Header: header with the same but with the third axis removed.
-        """
-        header = self.header.copy()
-        wcs = WCS(header)
-        wcs.sip = None
-        wcs = wcs.dropaxis(2)
-        header = wcs.to_header(relax=True)
-        return header
+        if old_axes_pos[dict_keys[0]] == 2:
+            new_data = new_data.swapaxes(0,2)
+            new_header = self.get_switched_header(0,2)
+            if old_axes_pos[dict_keys[1]] == 0:
+                new_data = new_data.swapaxes(1,0)
+                new_header = self.get_switched_header(1,0)
 
-test_data_cube = Data_cube(fits.open("gaussian_fitting/data_cubes/night_34_wcs.fits")[0])
+        elif old_axes_pos[dict_keys[0]] == 1:
+            new_data = new_data.swapaxes(0,1)
+            new_header = self.get_switched_header(0,1)
+            if old_axes_pos[dict_keys[1]] == 2:
+                new_data = new_data.swapaxes(0,2)
+                new_header = self.get_switched_header(0,2)
+
+        if old_axes_pos[dict_keys[1]] == 2:
+            new_data = new_data.swapaxes(1,2)
+            new_header = self.get_switched_header(1,2)
+        
+        return Data_cube(fits.PrimaryHDU(new_data, new_header), new_axes)
+
+    def get_switched_header(self, axis_1, axis_2) -> fits.header:
+        new_header = self.header.copy()
+        h_axis_1, h_axis_2 = axis_1+1, axis_2+1                             # The header uses 1-based indexing
+
+        for header_element in deepcopy(list(new_header.keys())):
+            if header_element[-1] == str(h_axis_1):
+                new_header[f"{header_element[:-1]}{h_axis_2}-"] = new_header.pop(header_element)
+            elif header_element[-1] == str(h_axis_2):
+                new_header[f"{header_element[:-1]}{h_axis_1}-"] = new_header.pop(header_element)
+        
+        # The modified header keywords are temporarily named with the suffix "-" to prevent duplicates during the process
+        # After the process is done, the suffix is removed
+        for header_element in deepcopy(list(new_header.keys())):
+            if header_element[-1] == "-":
+                new_header[header_element[:-1]] = new_header.pop(header_element)
+        
+        return new_header
+
+
+
+test_data_cube = Data_cube(fits.open("HI_regions/LOOP4_cube_bin2.fits")[0])
 # test_data_cube.bin_cube(4).rotate(45).rotate(30).plot_cube()
-test_data_cube.bin_cube_diagonally(4, 30).save_as_fits_file("bin.fits")
+# test_data_cube.bin_cube_diagonally(4, 30).save_as_fits_file("bin.fits")
 # test_data_cube.bin_cube(2).save_as_fits_file("bin.fits")
 
+inverted_data_cube = test_data_cube.switch_axes({"x": "v", "y": "b", "z": "l"})
+inverted_data_cube.save_as_fits_file("test_invert.fits")
+# test_data_cube.save_as_fits_file("test.fits")
