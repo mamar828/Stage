1ac2ad37734636984941d82c96697d372d393bdf
Mathieu Marquis <mamar828@ulaval.ca>
2024-07-26 09:32:06 -0400

    test_zfilter changed dir

--- src/tools/zurflueh_filter/test_zfilter.py
+++ src/tools/zurflueh_filter/test_zfilter.py
@@ -10,17 +10,18 @@ g = np.vectorize(lambda x, y: 50*np.exp(-((x-25)**2+(y-25)**2)/(2*10**2)))
 test_array_gaussian = g(*np.meshgrid(np.arange(50), np.arange(50))) + rand
 p = np.vectorize(lambda x, y: 0.1*x+0.03*y + 0.1*x**2+0.005*y**2)
 test_array_polynomial2 = p(*np.meshgrid(np.arange(50), np.arange(50))) + rand
-gradient = zfilter(test_array_gaussian)
+test_array_diagonal[20:25,20:25] = np.NAN
+gradient = zfilter(test_array_diagonal)
 
 titles = ["Random Array", "Added Gradient", "Test Array", "Filtered Array", "Filtered Gradient", "Random - Filtered"]
 figs = [gl.Figure(title=title) for title in titles]
 
 figs[0].add_elements(gl.Heatmap(rand, show_color_bar=True, vmin=-1.5, vmax=1.5))
-figs[1].add_elements(gl.Heatmap(test_array_gaussian - rand, show_color_bar=True))
-figs[2].add_elements(gl.Heatmap(test_array_gaussian, show_color_bar=True))
-figs[3].add_elements(gl.Heatmap(test_array_gaussian - gradient, show_color_bar=True, vmin=-1.5, vmax=1.5))
+figs[1].add_elements(gl.Heatmap(test_array_diagonal - rand, show_color_bar=True))
+figs[2].add_elements(gl.Heatmap(test_array_diagonal, show_color_bar=True))
+figs[3].add_elements(gl.Heatmap(test_array_diagonal - gradient, show_color_bar=True, vmin=-1.5, vmax=1.5))
 figs[4].add_elements(gl.Heatmap(gradient, show_color_bar=True))
-figs[5].add_elements(gl.Heatmap(rand - (test_array_gaussian-gradient), show_color_bar=True, vmin=-1.5, vmax=1.5))
+figs[5].add_elements(gl.Heatmap(rand - (test_array_diagonal-gradient), show_color_bar=True, vmin=-1.5, vmax=1.5))
 
 fig = gl.MultiFigure.from_grid(figs, (2,3), size=(14,9))
-fig.save("figures/zfilter/zurflueh_test_gaussian.pdf")
+fig.save("figures/zfilter/zurflueh_test_diagonal_2.pdf")
